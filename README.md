fb2f2683f6e4711eaa8436591a14aed347471155
Julio Cesar Montenegro <creativo@686studio.com>
2015-12-28 13:16:46 -0800

    local first commit

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# Los-Algodones-Fundraising-Event
+# Gulp and Bootstrap build project for sites
+
+## Prerequisites
+
+You will need the following things properly installed on your computer.
+
+* [Git](http://git-scm.com/)
+* [Node.js](http://nodejs.org/) (with NPM)
+
+## Usage
+
+Download:
+```
+git clone git@github.com:huleos/gulp-site-generator.git
+```
+
+Install:
+```
+npm i
+```
+
+Run:
+```
+gulp
+```
+Default task server, you can view it at: `http://localhost:3000`
---
+++ gulp/build.js
@@ -0,0 +1,23 @@
+/**********************************************
+ Build Task
+***********************************************/
+var gulp      = require('gulp'),
+		cache     = require('gulp-cache'),
+    del       = require('rimraf'),
+    sequence  = require('run-sequence');
+
+gulp.task('clean', function(done) {
+	return cache.clearAll(done)
+  del('./dist', done);
+});
+
+gulp.task('build', function(done) {
+  sequence('clean', ['pages', 'styles', 'scripts', 'images'], done);
+});
+
+gulp.task('default', ['build', 'server'], function() {
+	gulp.watch(['./src/**/*.html'], ['pages']);
+	gulp.watch('./src/assets/scss/**/*.scss', ['styles']);
+	gulp.watch(['./src/assets/js/*.js'], ['scripts']);
+	gulp.watch('./scr/assets/img/*', ['images']);
+});
